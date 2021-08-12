65a3fb68e31b6228f302d77b6bbd925ded116eb9
dimitar1128 <dimi.rangev09@gmail.com>
2021-08-13 01:38:55 +0300

    - fix urls for routing

--- websocket/OV_Wsocket/urls.py
+++ websocket/OV_Wsocket/urls.py
@@ -1,4 +1,4 @@
-"""OV_Wsocket URL Configuration
+"""OV_Webhook URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('event/', include('event.urls')),
 ]
