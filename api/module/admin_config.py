719d8f6d88242db30720d61159822defecfa2b64
dimitar1128 <dimi.rangev09@gmail.com>
2021-08-13 01:57:26 +0800

    api upload

---
+++ api/module/admin_config.py
@@ -0,0 +1,21 @@
+OV_APP_HOST_URL = '127.0.0.1:8000'
+
+DELAY_FOR_BAD_REQUEST = 0.5  # second
+
+BRIDGE_REDIS_CACHE_PREFIX = 'bridge'
+RATE_LIMIT_REDIS_CACHE_PREFIX = 'rate_limit'
+
+DEFAULT_MAX_ACTIVE_BRIDGES = 1
+DEFAULT_RATE_LIMIT_PER_URL = 14
+DEFAULT_ALLOWED_FREQUENCY = {
+    'af1': True,
+    'af2': True,
+    'af3': True,
+    'af4': True
+}
+DEFAULT_AVAILABLE_BRIDGE = {
+    'ab1': True,
+    'ab2': True,
+    'ab3': True,
+    'ab4': True
+}
---
+++ api/module/error.py
@@ -0,0 +1,2 @@
+RATE_LIMIT_PER_URL_EXCEED = 'Rate limit per url exceed.'
+INVALID_URL = 'Invalid URL.'
